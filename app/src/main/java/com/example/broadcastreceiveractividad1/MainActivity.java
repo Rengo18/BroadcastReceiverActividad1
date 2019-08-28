b91ee52adb4e5c8ebc6d3d7374b3ecd2bbd15b88
Franco Martin Colombo <fmc15308056@gmail.com>
2019-08-28 12:29:09 -0300

    colocando los permisos

--- app/src/main/java/com/example/broadcastreceiveractividad1/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiveractividad1/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.broadcastreceiveractividad1;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+
+
+public class MainActivity extends AppCompatActivity {
+    MensajeRecibido mensaje = new MensajeRecibido();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+        && checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS},1000);
+        }
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        MensajeRecibido mensaje = new MensajeRecibido();
+
         IntentFilter intel = new IntentFilter("provider.Telephony.SMS_RECEIVED");
         registerReceiver(mensaje,intel);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mensaje);
+    }
+
+
+
 }
